548b8734763bc322ed65ba1dc817006f01ecd115
Mohamed Emad <m.emad550@gmail.com>
2018-08-10 16:46:58 +0200

    change names

---
+++ src/components/Menu.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import '../../App.css'
+
+class Menu extends Component {
+
+    toggleMenu = () => {
+        const menu = document.querySelector('aside')
+        menu.classList.toggle("toggle")
+    }
+
+    render() {
+
+        return (
+            <header>
+                <h1 className="page-title">Neighborhood Map</h1>
+                <span className="menuIcon" onClick={this.toggleMenu}>&#9776;</span>
+            </header>
+        )
+    }
+}

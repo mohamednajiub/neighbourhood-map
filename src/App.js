18e471b9c8669e811c52d68e18c13f763b794786
Mohamed Emad <m.emad550@gmail.com>
2018-08-13 02:08:01 +0200

    add Maps Componet

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Menu from './components/Menu';
+import Map from './components/Map';
+
+
+
+
 class App extends Component {
+  
   render() {
-    return (
-      <div className="container">
-        <Header />
+    return ( 
+      <div className = "container" >
+        <Header/>
         <Menu />
+        <Map lat={30.12489} lng={31.29406} zoom={15}/>
       </div>
     );
   }
 }
+
 
 export default App;
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,2 +1,23 @@
-import React, { Component } from 'react';
-import '../../App.css'
+import React, {
+  Component
+} from 'react';
+import '../App.css';
+
+class Map extends Component {
+  shouldComponentUpdate() {
+    return false;
+  }
+  componentDidMount() {
+    this.map = new window.google.maps.Map(this.refs.map, {
+      center: { lat: this.props.lat, lng: this.props.lng},
+      zoom: this.props.zoom
+    })
+  }
+  render() {
+    return (
+      <div id = "map" ref = "map" />
+    )
+  }
+}
+
+export default Map

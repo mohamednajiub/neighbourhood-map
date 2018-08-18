85a62d67a0d65c1e7236a163ddf66b57406a0696
Mohamed Emad <m.emad550@gmail.com>
2018-08-18 22:31:46 +0200

    update Map component

--- src/App.js
+++ src/App.js
@@ -7,18 +7,43 @@ import Map from './components/Map';
 class App extends Component {
   state = { userLocation: { lat: 0, lng: 0 }, loading: true };
   componentDidMount(props) {
+    this.getUserLocation();
+  }
+  getUserLocation = () => {
     navigator.geolocation.getCurrentPosition(
       position => {
         const { latitude, longitude } = position.coords;
         this.setState({
           userLocation: { lat: latitude, lng: longitude },
           loading: false
+        },()=>{
+          this.getPlaces();
         });
       },
       () => {
         this.setState({ loading: false });
       }
     );
+  }
+  getPlaces = (query) => {
+    const API = 'https://api.foursquare.com/v2/venues/explore?';
+    const params = {
+      client_id: 'EICI1TY1WZY3KG2BGQCIMBETKC1TBOHYD1XP5RKIDSSEQZQF',
+      client_secret: 'PLBCQROLIXW3WWSOS1UK2XHCBCWQBRNBF0MB43CMUUQ4NBOT',
+      ll: this.state.userLocation,
+      query: query,
+      v: '20182570'
+    }
+
+    fetch(`${API}ll=${params.ll}&Id=4bf58dd8d48988d181941735,4bf58dd8d48988d13a941735&client_id=${params.client_id}&client_secret=${params.client_secret}&v=${params.v}`)
+    .then(res => {
+      if(!res.ok){
+        throw Error(res.statusText)
+      }
+      return res.json()
+    })
+    .then(response => console.log(response.response.groups[0]))
+    .catch(err => console.log('Foursquare API error: ', err))
   }
   render() {
     const { loading, userLocation } = this.state;
--- src/components/Map.js
+++ src/components/Map.js
@@ -6,6 +6,9 @@ class Map extends Component {
     return false;
   }
   componentDidMount() {
+    this.loadMap()
+  }
+  initMap = () => {
     this.map = new window.google.maps.Map(this.refs.map, {
       center: { lat: this.props.lat, lng: this.props.lng},
       zoom: this.props.zoom
@@ -17,12 +20,27 @@ class Map extends Component {
         lng: this.props.lng
       }
     });
+    let infowindow = new window.google.maps.InfoWindow();
+  }
+  loadMap = () =>{
+    loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyAgZzdo8v_0gAMHcFd_Act1gYhs9Klvc8o&callback=initMap');
+    // make the init map visible to React
+    window.initMap = this.initMap;
   }
   render() {
     return (
-      <div id = "map" ref = "map" />
+      <div id="map" ref="map" />
     )
   }
+}
+
+function loadScript(url) {
+  let index = window.document.getElementsByTagName('script')[0];
+  let script = window.document.createElement('script');
+  script.src = url;
+  script.async = true;
+  script.defer = true;
+  index.parentNode.insertBefore(script, index)
 }
 
 export default Map
